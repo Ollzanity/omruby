a5494ce795ee4b32991b803721e2e413af399d79
Oliver Carter <ollie.pat.carter@gmail.com>
2024-10-29 23:20:49 +0000

    fixed sass through esbuild and build.js file created

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,10 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
+// app/assets/config/manifest.js
+//= link_tree ../images
+//= link_directory ../builds .css
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
+//= link main.sass
